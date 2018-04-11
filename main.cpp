358ce0ca8a0dfabddc95684afbf16eb68b68179d
robinam <robinamixan@mail.ru>
2018-04-11 11:31:58 +0300

    lab2 ready

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <emmintrin.h>
+#include <x86intrin.h>
+
 using namespace std;
 
 int main() {
@@ -32,20 +35,70 @@ int main() {
         }
     }
 
+//    float** matr_res1 = new float*[size_matrix*size_matrix];
+//    for (int i = 0; i < size_matrix*size_matrix; ++i) {
+//        matr_res1[i] = new float [size_cell*size_cell];
+//        for (int j = 0; j < size_cell*size_cell; ++j) {
+//            matr_res1[i][j] = 0;
+//        }
+//    }
+
+    float mul_buff [4];
+    float column_buff [4];
+
     for (int x = 0; x < size_matrix; ++x) {
         for (int y = 0; y < size_matrix; ++y) {
             for (int r = 0; r < size_matrix; ++r) {
 
                 for (int i = 0; i < size_cell; ++i) {
                     for (int j = 0; j < size_cell; ++j) {
-                        for (int m = 0; m < size_cell; ++m) {
-                            matr_res[x * size_matrix + y][i * size_cell + j] += matr1[x * size_matrix + r][i * size_cell + m] * matr2[r * size_matrix + y][m * size_cell + j];
+                        __m128 buffer = _mm_setzero_ps();
+                        for (int m = 0; m< size_cell; m+=4 ) {
+                            for (int l = 0; l < 4; l++) {
+                                column_buff[l] = matr2[r * size_matrix + y][(m+l) * size_cell + j];
+                            }
+//                            __m128 a = _mm_load_ps(&matr1[r * size_matrix + y][m * size_cell + j]);
+                            __m128 a = _mm_load_ps(&column_buff[0]);
+                            __m128 b = _mm_load_ps(&matr1[x * size_matrix + r][i * size_cell + m]);
+                            __m128 mul = _mm_mul_ps(a, b);
+                            buffer = _mm_add_ps(mul, buffer);
                         }
+                        buffer = _mm_add_ps(buffer, buffer);
+                        buffer = _mm_add_ps(buffer, buffer);
+                        _mm_store_ss(&matr_res[x * size_matrix + y][i * size_cell + j], buffer);
+//                         += mul_buff[0] + mul_buff[1] + mul_buff[2] + mul_buff[3];
                     }
                 }
+
             }
         }
     }
+
+//    for (int x = 0; x < size_matrix; ++x) {
+//        for (int y = 0; y < size_matrix; ++y) {
+//            for (int r = 0; r < size_matrix; ++r) {
+//
+//                for (int i = 0; i < size_cell; i++) {
+//                    for (int j = 0; j < size_cell; j++) {
+//                        for (int m = 0; m< size_cell; m++) {
+//                            matr_res1[x * size_matrix + y][i * size_cell + j] += matr1[x * size_matrix + r][i * size_cell + m] * matr2[r * size_matrix + y][m * size_cell + j];
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+
+//    for (int j = 0; j < size_cell; ++j) {
+//        __m128 buffer = _mm_setzero_ps();
+//        for (int m = 0; m< size_cell; ++m) {
+//            __m128 a = _mm_load_ss(&matr2[r * size_matrix + y][m * size_cell + j]);
+//            __m128 b = _mm_load_ss(&matr1[x * size_matrix + r][i * size_cell + m]);
+//            buffer = _mm_add_ps(_mm_mul_ss(a, b), buffer);
+//        }
+//        _mm_store_ss(&matr_res[x * size_matrix + y][i * size_cell + j], buffer);
+//    }
 
     clock_t end = clock();
     double seconds = (double)(end - start) / CLOCKS_PER_SEC;
---
+++ main_cache.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <emmintrin.h>
+#include <unistd.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    clock_t start = clock();
+    srand(time(0));
+
+    int cache_size = sysconf(_SC_LEVEL3_CACHE_SIZE );
+    int block_size = (cache_size / 3) * 0.8;
+    int elem_amount = block_size / 4;
+    int size_cell = (int)(sqrt(elem_amount) / 32) * 32;
+
+//    int size_cell = 300;
+
+    const int count_columns = 4;
+
+    const int size_matr = size_cell * count_columns;
+
+    float** matr1 = new float*[size_matr];
+    float** matr2 = new float*[size_matr];
+    float** matr_res = new float*[size_matr];
+    float** matr_res1 = new float*[size_matr];
+
+    for (int i = 0; i < size_matr; ++i) {
+
+        matr1[i] = new float [size_matr];
+        matr2[i] = new float [size_matr];
+        matr_res[i] = new float [size_matr];
+        matr_res1[i] = new float [size_matr];
+
+        for (int j = 0; j < size_matr; ++j) {
+            matr1[i][j] = random()%4;
+            matr2[i][j] = random()%4;
+            matr_res[i][j] = 0;
+            matr_res1[i][j] = 0;
+        }
+    }
+
+//    for (int x = 0; x < count_columns; ++x) {
+//        for (int r = 0; r < count_columns; ++r) {
+//            for (int y = 0; y < count_columns; ++y) {
+//
+//
+//                for (int i = 0; i < size_cell; i++) {
+//                    for (int j = 0; j < size_cell; j++) {
+//                        for (int m = 0; m < size_cell; m++) {
+//
+//                            int matr1_x = i + size_cell * x;
+//                            int matr_m = m + size_cell* r;
+//                            int matr2_y = j + size_cell * y;
+//
+//                            matr_res[matr1_x][matr2_y] += matr1[matr1_x][matr_m] * matr2[matr_m][matr2_y];
+////                            cout << matr1[matr1_x][matr_m] << "[" << matr1_x << matr_m << "]";
+////                            cout<< "*" << matr2[matr_m][matr2_y] << "[" << matr_m << matr2_y << "]" << endl;
+//                        }
+//                    }
+//                }
+//                cout << x << r << " * " << r << y << " = " << x << y << endl;
+//            }
+//        }
+//    }
+
+
+    for (int x = 0; x < count_columns; ++x) {
+        for (int r = 0; r < count_columns; ++r) {
+            for (int y = 0; y < count_columns; ++y) {
+
+                for (int i = 0; i < size_cell; i++) {
+                    for (int j = 0; j < size_cell; j++) {
+                        for (int m = 0; m < size_cell; m++) {
+
+                            int matr1_x = i + size_cell * x;
+                            int matr_m = m + size_cell* r;
+                            int matr2_y = j + size_cell * y;
+
+                            matr_res[matr1_x][matr2_y] += matr1[matr1_x][matr_m] * matr2[matr_m][matr2_y];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+//    for (int i = 0; i < size_matr; i++) {
+//        for (int j = 0; j < size_matr; j++) {
+//            for (int m = 0; m < size_matr; m++) {
+//                matr_res1[i][j] += matr1[i][m] * matr2[m][j];
+//            }
+//        }
+//    }
+
+
+    clock_t end = clock();
+    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+
+    cout << "runtime = " << seconds << endl;
+    return 0;
+}
